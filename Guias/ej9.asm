a26aedc3ff4a04a5eaab1fd0bdcc47463364384d
fnpratto <fnpratto@gmail.com>
2024-07-09 21:26:31 -0300

    ej 9 terminado

--- Guias/ej9.asm
+++ Guias/ej9.asm
@@ -25,13 +25,28 @@ extern gets
 extern printf
 
 section .data
-    matriz dq 1, 2, 3, 4, 5
-           dq 0, 1, 2, 3, 4
-           dq 0, 0, 1, 2, 3
-           dq 0, 0, 0, 1, 2
-           dq 0, 0, 0, 0, 1
+    matriz1 dq 1, 2, 3, 4, 5
+            dq 0, 1, 2, 3, 4
+            dq 0, 0, 1, 2, 3
+            dq 0, 0, 0, 1, 2
+            dq 0, 0, 0, 0, 1 ; sup
+
+    matriz2 dq 0, 0, 0, 0, 0
+            dq 0, 0, 0, 0, 0
+            dq 0, 0, 0, 0, 0
+            dq 0, 0, 0, 0, 0
+            dq 0, 0, 0, 0, 0  ; las dos
+
+    matriz3 dq 1, 0, 0, 0, 0
+            dq 1, 1, 0, 0, 0
+            dq 1, 1, 1, 0, 0
+            dq 1, 1, 1, 1, 0
+            dq 1, 1, 1, 1, 1 ; inferior 
+
     mensajeSup db 'La matriz es triangular superior', 0
+    mensajeNoSup db 'La matriz NO es triangular superior', 0
     mensajeInf db 'La matriz es triangular inferior', 0
+    mensajeNoInf  db 'La matriz NO es triangular inferior', 0
     mensajeNoTriangular db 'La matriz no es triangular', 0
     longFila     dq 40
     longElemento dq 8
@@ -47,113 +62,80 @@ section .bss
 
 section     .text
 main:
-    mov byte [esTriangularSup], 1 ; seteo trye 
-    mov byte [esTriangularInf], 1
+comprobarTriangularInferior:
+    mov rsi, matriz3  
+    add rsi, [contadorFil] 
+    inc rsi
+
+    mov qword[contColLocal] ,0
+    mov rax, [contColLocal] ; cantidad de col a chequear por cada fil
+    add rax, [numeroFilas] ; +5
+    sub rax, [contadorFil] ; - fila en la que esta
+    dec rax
+    mov [contColLocal], rax
+
+comprobarFilaInferior:
+    mov rdx, [contadorCol]   
+    cmp rdx, [contColLocal],
+    jge siguienteFilaInferior
+    mov rax, [rsi + rdx*8]
+    cmp rax, 0
+    jne noEsTriangularInferior
+    add rdx, 1
+    mov [contadorCol], rdx
+    jmp comprobarFilaInferior
+
+noEsTriangularInferior:
+    mov rdi, mensajeNoInf
+    mPuts
+    jmp resetVariables
+
+siguienteFilaInferior:
+    mov rbx, [contadorFil] 
+    add rbx, 1
+    mov [contadorFil], rbx
+    cmp rbx, [numeroFilas]
+    jl comprobarTriangularInferior
+    mov rdi, mensajeInf
+    mPuts
+;;;;;;;;
+resetVariables:
+    mov qword[contadorFil] ,0
+    mov qword[contColLocal] ,0
+    mov qword[contadorCol] ,0
 
 comprobarTriangularSuperior:
-    mov rdx, [contadorFil]         ; contador de columnas = número de fila
-    mov rsi, matriz     
+    mov rsi, matriz3 
 
-    mov rax, [contColLocal] ; cantidad de col a chequear por cada fil
-    inc rax, [numeroFilas] ; +5
-    dec rax, [contadorFil] ; - fila en la que esta
-    mov [contadorCol], rax
+    mov qword[contColLocal] ,0
+    mov rax, qword[contColLocal]
+    add rax, [contadorFil] ; 
+    mov [contColLocal], rax
 
 comprobarFilaSuperior:
-    cmp rdx, [contadorCol],; comparo 
+    mov rdx, [contadorCol]   
+    cmp rdx, [contColLocal],
     jge siguienteFilaSuperior
     mov rax, [rsi + rdx*8]
     cmp rax, 0
     jne noEsTriangularSuperior
     add rdx, 1
-    mov [contadorFil], rdx
+    mov [contadorCol], rdx
     jmp comprobarFilaSuperior
 
 noEsTriangularSuperior:
-    mov byte [esTriangularSup], 0
-    jmp comprobarTriangularInferior
-
-siguienteFilaSuperior:
-    add rsi, [longFila]
-    mov rbx, [contadorCol] 
-    add rbx, 1
-    mov [contadorCol], rbx
-    cmp rbx, [numeroFilas]
-    jl comprobarTriangularSuperior
-    mov rdi, msjSup
+    mov rdi, mensajeNoSup
     mPuts
     jmp fin
 
-
-comprobarTriangularInferior:
-
-
-    ; Comprobar si es triangular inferior
-    mov ecx, 5           ; número de filas
-    mov esi, matriz      ; puntero a la matriz
-    mov ebx, 0           ; contador de filas
-
-comprobarTriangularInferior:
-    mov rax,rbx
-    imul 4
-    mov ebx, rax
-    add esi, ebx       ; puntero a la diagonal actual
-    mov edx, 0           ; contador de columnas
-comprobarFilaInferior:
-    cmp edx, ebx
-    jge siguienteFilaInferior
-    mov eax, [esi + edx*4]
-    cmp eax, 0
-    jne noEsTriangularInferior
-    add edx, 1
-    jmp comprobarFilaInferior
-
-noEsTriangularInferior:
-    mov byte [esTriangularInf], 0
-
-siguienteFilaInferior:
-    ; 20 - (ebx+1)*4
-    mov rax, ebx
-    inc rax, 1
-    imul 4
-    mov rdi, 20
-    dec rdi, rax
-
-    add esi, rdi
-    add ebx, 1
-    cmp ebx, 5
-    jl comprobarTriangularInferior
-
-    ; Imprimir el resultado
-    mov eax, 4           ; syscall number for sys_write
-    mov ebx, 1           ; file descriptor 1 (stdout)
-
-    ; Imprimir si es triangular superior
-    mov al, [esTriangularSup]
-    cmp al, 1
-    jne comprobarInferior
-    mov ecx, mensajeSup
-    mov edx, 30
-    int 0x80
-
-comprobarInferior:
-    ; Imprimir si es triangular inferior
-    mov al, [esTriangularInf]
-    cmp al, 1
-    jne noTriangular
-    mov ecx, mensajeInf
-    mov edx, 30
-    int 0x80
-    jmp fin
-
-noTriangular:
-    ; Imprimir que no es triangular
-    mov ecx, mensajeNoTriangular
-    mov edx, 28
-    int 0x80
+siguienteFilaSuperior:
+    mov rbx, [contadorFil] 
+    add rbx, 1
+    mov [contadorFil], rbx
+    cmp rbx, [numeroFilas]
+    jl comprobarTriangularSuperior
+    mov rdi, mensajeSup
+    mPuts
 
 fin:
-    ; Exit
-    mov eax, 1           ; syscall number for sys_exit
-    xor ebx, ebx         ; exit code 0
-    int 0x80
+    ret
